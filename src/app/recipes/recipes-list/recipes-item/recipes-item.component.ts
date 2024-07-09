cafaddf7e872bd198ebd00c0af06093d6cbbaf45
GeorgeThulo <Ofentsethulo@hotmail.com>
2024-07-09 16:31:05 +0200

    Using the @input decorator to use the item in the correct component

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipes.model';
 
 @Component({
   selector: 'app-recipes-item',
   templateUrl: './recipes-item.component.html',
   styleUrl: './recipes-item.component.css'
 })
-export class RecipesItemComponent {
+export class RecipesItemComponent implements OnInit{
+@Input() recipe!: Recipe;
 
+
+  constructor() {}
+  ngOnInit(){
+    
+  }
 }
